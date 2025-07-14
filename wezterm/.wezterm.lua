436d1f264301ef1934ab6349c7d9ff8d07f24ad0
cjnovak98 <notmy_email@example.com>
2025-07-14 19:18:14 -0400

    initial WezTerm configs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,2 +1,10 @@
-wezterm.config.tab_bar_at_bottom = true
+local wezterm = require("wezterm") 
 
+local config = {}
+
+if wezterm.config_builder then config = wezterm.config_builder() end
+
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
+
+return config
